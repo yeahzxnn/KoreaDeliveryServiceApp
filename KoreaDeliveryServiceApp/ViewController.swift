c7e7b40478d3a0e1f58167c7e23aed46988ed4ff
yeahzxnn <yejin0621@tukorea.ac.kr>
2022-12-17 23:52:05 +0900

    Initial Commit

---
+++ KoreaDeliveryServiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KoreaDeliveryServiceApp
+//
+//  Created by 신예진 on 2022/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KoreaDeliveryServiceAppUITests/KoreaDeliveryServiceAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KoreaDeliveryServiceAppUITestsLaunchTests.swift
+//  KoreaDeliveryServiceAppUITests
+//
+//  Created by 신예진 on 2022/12/17.
+//
+
+import XCTest
+
+final class KoreaDeliveryServiceAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
